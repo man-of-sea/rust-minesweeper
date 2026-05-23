b42aad016cddc2328db2dcb7507fbb27d0c98165
Szymon Raczkowski <duodexima@gmail.com>
2026-05-24 01:49:03 +0200

    Added difficulty enum

---
+++ src/difficulty.rs
@@ -0,0 +1,23 @@
+pub enum Difficulty {
+    Beginner,
+    Intermediate,
+    Expert
+}
+
+impl Difficulty {
+    pub fn settings(&self) -> (usize, usize, usize) {
+        match self {
+            Difficulty::Beginner     => (9,  9,  10),
+            Difficulty::Intermediate => (16, 16, 40),
+            Difficulty::Expert       => (16, 30, 99)
+        }
+    }
+
+    pub fn label(&self) -> &str {
+        match self {
+            Difficulty::Beginner     => "Beginner",
+            Difficulty::Intermediate => "Intermediate",
+            Difficulty::Expert       => "Expert"
+        }
+    }
+}
